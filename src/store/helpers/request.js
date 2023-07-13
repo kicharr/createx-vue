6485c900b272189ed875f5af4aed5bd37243ba60
kicharr <99511093+kicharr@users.noreply.github.com>
2023-07-13 19:34:10 +0300

    first commit

---
+++ src/store/helpers/request.js
@@ -0,0 +1,15 @@
+
+// makes post request to server
+export async function makePostReq(url, data, headers) {
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            data ? {"Content-Type": "application/json"} : null,
+            headers
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch(url, requestOptions);
+    const responseData = await response.json();
+    const postId = responseData.id;
+}
